854aa6368be708bec8fd2c2bbc9273f0d992e913
samuel mulet molina <sambledo@hotmail.com>
2018-06-17 15:39:56 +0200

    creación del programa

---
+++ trabajo_repos/trabajo_repos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo_repos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
