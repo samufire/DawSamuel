622364f8f9fcf5c20304cc4d4aa866d85b22c9dd
samuel mulet molina <sambledo@hotmail.com>
2018-06-17 15:44:38 +0200

    Crear rama 1

---
+++ trabajo_repos/trabajo_repos/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo_repos
+{
+    class Class1
+    {
+        public double Suma(double a, double b) {
+            double c;
+            c = a + b;
+            Console.WriteLine("Suma: "+c);
+            return c;
+        }
+
+        public double Resta(double a, double b)
+        {
+            double c;
+            c = a - b;
+            Console.WriteLine("Resta: " + c);
+            return c;
+        }
+
+    }
+}
