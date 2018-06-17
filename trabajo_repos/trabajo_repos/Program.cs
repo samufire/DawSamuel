835444a36a82c6d6e66480ed2de57e4eaf664caa
samuel mulet molina <sambledo@hotmail.com>
2018-06-17 15:53:31 +0200

    Crear program

--- trabajo_repos/trabajo_repos/Program.cs
+++ trabajo_repos/trabajo_repos/Program.cs
@@ -10,6 +10,17 @@ namespace trabajo_repos
     {
         static void Main(string[] args)
         {
+            Class1 c1 = new Class1();
+            Class2 c2 = new Class2();
+
+            c2.Mul(10, 5);
+            c2.Div(5, 2);
+
+            c1.Suma(3, 4);
+            c1.Resta(5, 2);
+
+            Console.ReadKey();
+
 
         }
     }
