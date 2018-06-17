5aa26cb8c854877509e75776a62249792bf95eee
samuel mulet molina <sambledo@hotmail.com>
2018-06-17 15:47:09 +0200

    Crear Rama 2

---
+++ trabajo_repos/trabajo_repos/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo_repos
+{
+    class Class2
+    {
+        public double Mult(double a, double b)
+        {
+            double c;
+            c = a * b;
+            Console.WriteLine("Mult: " + c);
+            return c;
+        }
+        public double Div(double a, double b)
+        {
+            double c;
+            c = a / b;
+            Console.WriteLine("Div: " + c);
+            return c;
+        }
+    }
+}
